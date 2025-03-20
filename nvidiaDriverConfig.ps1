aa6484f6800db4a549af36134e45ccb73cd2ade5
BySaDaS <147529310+BySaDaS@users.noreply.github.com>
2025-03-20 20:00:46 +0100

    Update nvidiaDriverConfig.ps1

--- nvidiaDriverConfig.ps1
+++ nvidiaDriverConfig.ps1
@@ -1,5 +1,5 @@
 ﻿if ( -not (Test-Path "C:\Users\Public\nvidiaProfileInspector")) {
-Invoke-WebRequest -Uri "https://github.com/Orbmu2k/nvidiaProfileInspector/releases/download/2.4.0.4/nvidiaProfileInspector.zip" -OutFile "C:\Users\Public\nvidiaProfileInspector.zip"
+Invoke-WebRequest -Uri "https://github.com/Orbmu2k/nvidiaProfileInspector/releases/download/2.4.0.19/nvidiaProfileInspector.zip" -OutFile "C:\Users\Public\nvidiaProfileInspector.zip"
 Expand-Archive C:\Users\Public\nvidiaProfileInspector.zip C:\Users\Public\nvidiaProfileInspector
 Remove-Item C:\Users\Public\nvidiaProfileInspector.zip -Force
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/BySaDaS/nvidiaProfileInspector/main/my_profile.nip" -OutFile C:\users\public\nvidiaprofileinspector\my_profile.nip
